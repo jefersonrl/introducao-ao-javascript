cd09f38adb106e38b4a4d7c9fcea92001884a864
jefersonrl <jefersonrl@icloud.com>
2018-08-16 21:54:21 -0300

    Aula de javascript

---
+++ js/funcoes.js
@@ -0,0 +1,7 @@
+function trocaImagem() {
+	document.getElementById("myImage").src="img/html-logo-badge-512.png";
+}
+
+function imagemTroca() {
+	document.getElementById("myImage").src="img/js-logo-badge-512.png";
+}
