e5ab47ef72b2960078879f8ccd6a5f77c802dd0c
jefersonrl <jefersonrl@icloud.com>
2018-08-23 21:20:00 -0300

    Alterando a fonte com CSS

--- js/funcoes.js
+++ js/funcoes.js
@@ -6,6 +6,17 @@ function imagemTroca() {
 	document.getElementById("myImage").src="img/js-logo-badge-512.png";
 }
 
-function trocaFonte() {
-	document.getElementById("demo").style.fontSize="35px"
+function trocaFonteMaior() {
+	document.getElementById("demo").style.fontSize="35px";
+}
+
+function trocaFonteMenor() {
+	document.getElementById("demo").style.fontSize="16px";
+}
+
+function olcutarElemento(){
+	document.getElementById("demo").style.display="none";
+}
+function naoOlcutarElemento(){
+	document.getElementById("demo").style.display="block";
 }
